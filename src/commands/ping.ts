e6331c3da4e34ad832754f489dedf24a4ab8ae18
Chloe <solely@riseup.net>
2023-04-03 12:47:54 +0000

    refactor: ping command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -3,8 +3,12 @@ import { defineCommand } from '../lib/Command';
 export default defineCommand({
   name: 'ping',
   description: 'Pings and checks the latency of the bot.',
+  aliases: ['pong'],
   category: 'util',
-  async run(ctx: any) {
-    await ctx.send('Pong!');
+  async run(ctx) {
+    const message = await ctx.send('Ping...');
+    const ping = Math.floor(message.createdAt - ctx.msg.createdAt);
+
+    return message.edit(`Pong! Took ${ping} ms.`);
   },
 });
